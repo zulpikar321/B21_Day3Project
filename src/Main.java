bdc291c88dc33ebbe4ed9f191197c5508bf4cb6a
zulpikar321 <zulpikarzulay@gmail.com>
2020-11-12 22:47:49 -0800

    B21 is dedicated

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git Last Day");
-        // B21 is exciting
+        // B21 is exciting !!!
+        // B21 is dedicated
     }
 }
